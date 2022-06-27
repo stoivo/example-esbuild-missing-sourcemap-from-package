fa30a9f327361a5b02be845cf7fb3f75a0e967b3
Simon <simon.toivo@telhaug.no>
2022-06-27 13:29:35 +0200

    workspace

--- script/esbuild.js
+++ script/esbuild.js
@@ -2,9 +2,10 @@ import * as esbuild from 'esbuild';
 
 esbuild
   .build({
-    entryPoints: ['src/index.ts', ],
+    entryPoints: ['src/index.ts', 'src/main.ts', ],
     outbase: 'src',
     outdir: 'dist',
+    bundle: true,
     sourcemap: true,
     logLevel: 'debug',
     tsconfig: 'tsconfig.json',
